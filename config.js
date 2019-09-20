2caa5a0eeb6c736dc19306d1e9eb81a3eca733db
Horea Porutiu <horeaporutiu@gmail.com>
2019-09-20 16:45:21 -0700

    add config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "x-ibm-client-secret": 'your-client-secret-goes-here',
+  "x-ibm-user-secret": 'your-user-secret-goes-here',
+  "x-ibm-client-id": 'your-client-id-goes-here'
+}
