26a29e9b24bdeda04c22d8e9a7d26d333da2da46
Horea Porutiu <horeaporutiu@gmail.com>
2019-10-03 01:37:30 -0700

    update config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "x-ibm-client-secret": 'your-client-secret-goes-here',
-  "x-ibm-user-secret": 'your-user-secret-goes-here',
-  "x-ibm-client-id": 'your-client-id-goes-here'
-}
