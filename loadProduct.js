4bccfad9a9de559c1eaa0794ef0271c4cc30c89f
Horea Porutiu <horeaporutiu@gmail.com>
2019-10-03 01:35:52 -0700

    update load product

--- loadProduct.js
+++ loadProduct.js
@@ -1,30 +1,78 @@
-var request = require("request");
-let productInfo = require("./productInfo");
-const config = require('./config.js');
+'use strict';
 
-var options = {
-  method: 'POST',
-  url: 'https://api.ibm.com/scinsights/run/api/products',
-  headers:
-  {
-    'content-type': 'application/json',
-    'x-ibm-client-secret': config.x-ibm-client-secret,
-    'x-ibm-user-secret': config.x-ibm-user-secret,
-    'x-ibm-client-id': config.x-ibm-client-id 
-   },
-  body: productInfo,
-  json: true
-};
+var request = require("request-promise");
+const path = require('path');
+const fs = require('fs');
+var util = require('util');
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+//connect to the config file
+const configPath = path.join(process.cwd(), './config.json');
+const configJSON = fs.readFileSync(configPath, 'utf8');
+const config = JSON.parse(configJSON);
 
-  console.log('statusCode: ')
-  console.log(response.statusCode);
+//connect to the product info file
+const productPath = path.join(process.cwd(), './sampleProduct.json');
+const productJSON = fs.readFileSync(productPath, 'utf8');
+const productInfo = JSON.parse(productJSON);
 
-  console.log(body)
+async function main() {
+
+  await addProductRequest();
+  console.log('after product request')
+
+  getProductById(productInfo._id)
 
 
-});
+}
 
+async function addProductRequest() {
+
+  var addProductRequest = {
+    method: 'POST',
+    url: 'https://api.ibm.com/scinsights/run/api/products',
+    headers:
+    {
+      'content-type': 'application/json',
+      'x-ibm-client-secret': config.clientSecret,
+      'x-ibm-user-secret': config.userSecret,
+      'x-ibm-client-id': config.clientId 
+     },
+    body: productInfo,
+    json: true
+  };
+
+  
+
+  await request(addProductRequest);
+  console.log(`addProductRequest successful`);
+
+}
+
+async function getProductById(id) {
+
+  console.log('id: ')
+  console.log(id)
+
+  var getProductById = {
+    method: 'GET',
+    url: 'https://api.ibm.com/scinsights/run/api/products/' + id,
+    headers:
+    {
+      'content-type': 'application/json',
+      'x-ibm-client-secret': config.clientSecret,
+      'x-ibm-user-secret': config.userSecret,
+      'x-ibm-client-id': config.clientId 
+     },
+    json: true
+  };
+
+  let res = await request(getProductById);
+  console.log(`getProductById response: `);
+
+  console.log(res)
+
+}
+
+
+main()
 
